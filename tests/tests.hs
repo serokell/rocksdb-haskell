f0175765dd98aa2753dab4d4cc76d11b1812d570
Alexandre <alexandrer_b@outlook.com>
2017-11-24 03:24:14 +0000

    [CSL-1803] Add test to database with Unicode in its filepath

--- tests/tests.hs
+++ tests/tests.hs
@@ -1,18 +1,20 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE BinaryLiterals      #-}
+{-# LANGUAGE OverloadedStrings   #-}
 
 module Main where
 
 import           Control.Monad.IO.Class       (MonadIO (liftIO))
 import           Control.Monad.Trans.Resource (MonadResource, runResourceT)
-import           Data.Default
-import           System.Process               (system)
+import           Data.Char                    (chr)
+import           Data.Default                 (def)
 import           System.IO.Temp               (withSystemTempDirectory)
-import           Test.Hspec
-import           Test.Hspec.Expectations
-import           Test.Hspec.QuickCheck        (prop)
-import           Test.QuickCheck
 
-import           Database.RocksDB
+import           Database.RocksDB             (Compression (..), DB, compression,
+                                               createIfMissing, defaultOptions, get, open,
+                                               put)
+
+import           Test.Hspec                   (describe, hspec, it, shouldReturn)
+import           Test.QuickCheck              (Arbitrary (..), elements, generate, listOf)
 
 initializeDB :: MonadResource m => FilePath -> m DB
 initializeDB path =
@@ -21,6 +23,19 @@ initializeDB path =
         defaultOptions
         {createIfMissing = True, compression = NoCompression}
 
+newtype HardString = HardString {getHardString :: FilePath}
+  deriving (Show, Eq)
+
+instance Arbitrary HardString where
+    arbitrary =
+        HardString <$> (listOf . elements . map chr $ notlatin)
+       where
+         -- This is not intended to be an exhaustive of nonlatin Unicode
+         -- alphabets/syllabaries
+         notlatin = japanese ++ russian
+         japanese = [0x3040..0x309F] ++ [0x30A0..0x30FF]
+         russian  = [0x0410..0x044F]
+
 main :: IO ()
 main =  hspec $ do
 
@@ -28,6 +43,16 @@ main =  hspec $ do
     it "should put items into the database and retrieve them" $  do
       runResourceT $ withSystemTempDirectory "rocksdb" $ \path -> do
         db <- initializeDB path
+
+        put db def "zzz" "zzz"
+        get db def "zzz"
+      `shouldReturn` (Just "zzz")
+
+    it "should put items into a database whose filepath has unicode characters and\
+       \ retrieve them" $  do
+      runResourceT $ withSystemTempDirectory "rocksdb" $ \path -> do
+        unicode <- liftIO $ generate arbitrary
+        db <- initializeDB $ getHardString unicode ++ path
 
         put db def "zzz" "zzz"
         get db def "zzz"
