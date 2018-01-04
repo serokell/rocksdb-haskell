bab1bb02f16ab56b861d1c4ecf0a6a9ed0f71cab
Niklas Hambüchen <mail@nh2.me>
2018-01-04 01:10:01 +0100

    tests: Add positive-regresion check for LOCK check behaviour

--- tests/tests.hs
+++ tests/tests.hs
@@ -3,17 +3,20 @@
 
 module Main where
 
+import           Control.Monad                (when)
 import           Control.Monad.IO.Class       (MonadIO (liftIO))
 import           Control.Monad.Trans.Resource (runResourceT)
 import           Data.Default                 (def)
+import           System.Directory             (createDirectoryIfMissing, doesDirectoryExist, removeDirectoryRecursive)
 import           System.FilePath              ((</>))
+import           System.IO.Error              (ioeGetErrorType, isUserErrorType, ioeGetErrorString)
 import           System.IO.Temp               (withSystemTempDirectory)
 
 import           Database.RocksDB             (Compression (..), DB, compression,
                                                createIfMissing, defaultOptions, get, open,
                                                put, close)
 
-import           Test.Hspec                   (describe, hspec, it, shouldReturn)
+import           Test.Hspec                   (describe, hspec, it, shouldReturn, shouldThrow)
 import           Test.QuickCheck              (Arbitrary (..), UnicodeString (..),
                                                generate)
 
@@ -36,6 +39,32 @@ main =  hspec $ do
         close db
         return val
       `shouldReturn` (Just "zzz")
+
+    it "does weird global singleton string matching stuff for double-locking warnings" $ do
+      -- This test will fail when RocksDB fixes this issue
+      -- (see https://stackoverflow.com/questions/37310588/rocksdb-io-error-lock-no-locks-available#comment83145041_37312033).
+      -- It exists so that we get notified when that fixing happens.
+      let path = "/tmp/tmpdir"
+
+      exists <- doesDirectoryExist path
+      when exists $ removeDirectoryRecursive path
+
+      createDirectoryIfMissing True path
+
+      (runResourceT $ do
+        db <- initializeDB path
+        put db def "zzz" "zzz"
+        get db def "zzz") `shouldReturn` (Just "zzz")
+      -- We purposely don't `close` the DB above.
+
+      removeDirectoryRecursive path
+
+      (runResourceT $ do
+        db <- initializeDB path
+        put db def "zzz" "zzz"
+        get db def "zzz") `shouldThrow` \ioe ->
+          isUserErrorType (ioeGetErrorType ioe) &&
+          ioeGetErrorString ioe == ("open: IO error: lock " ++ path ++ "/LOCK: No locks available")
 
     it "should put items into a database whose filepath has unicode characters and\
        \ retrieve them" $  do
